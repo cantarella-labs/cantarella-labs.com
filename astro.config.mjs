a31cb2a6fac8eb0d44f211c550de6d647b06e337
vcantarella <vcantarella@gmail.com>
2026-06-10 15:36:00 +0200

    CNAME testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.cantarellalabs.com',
+    // base: '/your-repo-name', // ONLY if NOT using a custom domain and repo isn't <username>.github.io
+  });
